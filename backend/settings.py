3a5edc25f4f854d250c1b268226d62b8364db665
medoledo <medoledo144@gmail.com>
2025-09-12 00:50:36 +0300

    hehe

--- backend/settings.py
+++ backend/settings.py
@@ -101,6 +101,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
